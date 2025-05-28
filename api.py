b2c5ae5755ed5a2abd606cb9ede8988e88ab7a8b
Titan007 <sanskargambhir93@gmail.com>
2025-05-28 23:39:45 +0530

    hiding apikey

--- api.py
+++ api.py
@@ -1,6 +1,13 @@
 import google.generativeai as genai
+from dotenv import load_dotenv
+import os
 
-genai.configure(api_key="AIzaSyDYIFYLQj1qa91dV3tnCDR0OHAUVyJwyTw")
+load_dotenv()  # load variables from .env into environment
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("How long is burz khalifa")
 print(response.text)
